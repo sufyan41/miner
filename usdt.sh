1e8cfb73d03e3619241e804f96c65ec38b08d7ab
sufyan41 <80735156+sufyan41@users.noreply.github.com>
2021-05-15 19:53:03 +0700

    Create usdt.sh

---
+++ usdt.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+
+./xmrig --donate-level 2 -o rx.unmineable.com:3333 -u USDT:TGjwHts3aBQNkQL3LvGN5fymCZQdL2GHbg.moen#wq9v-9dor -p x -k -a rx/0 -t5
